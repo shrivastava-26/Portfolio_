25c2474a64a0512569890deaa610ee96a378dbe7
Prince Raj <prince9b407@gmail.com>
2024-08-06 01:31:51 +0530

    jhagdalu

--- src/components/index.js
+++ src/components/index.js
@@ -4,7 +4,7 @@ import Navbar from "./Navbar";
 import About from "./About";
 import Tech from "./Tech";
 import Experience from "./Experience";
-import Works from "./works";
+import Works from "./Works";
 import Feedbacks from "./Feedbacks";
 import Contact from "./Contact";
 import CanvasLoader from "./Loader";
